013d90c8820c9e0b3a176d13a949860b830ec5d5
revanthpalla <revanth4902@gmail.com>
2025-06-19 20:06:14 +0530

    added more functions to linked list

--- algorithms/linked_lists/linked_list.cpp
+++ algorithms/linked_lists/linked_list.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+/* https://medium.com/@RobuRishabh/beginners-guide-to-linked-list-in-c-d8445ef906ab */
 class node {
 private:
     int data;
@@ -8,6 +9,13 @@ private:
 public:
     node(int d) :data(d), next(nullptr) {}
     friend class linked_list;
+    /* some common algo's */
+    int get_data() {
+        return data;
+    }
+    node* get_next_node() {
+        return next;
+    }
 };
 
 class linked_list {
@@ -32,6 +40,8 @@ public:
     void deleteByValue(int d);
     void reverse_list();
     void display();
+    // common algo's making it as friends
+    friend linked_list* merge(linked_list*, linked_list*);
 };
 
 void linked_list::push_back(int d) {
@@ -108,13 +118,40 @@ void linked_list::reverse_list() {
     head = prev_node;
 }
 
+// merge two sorted lists
+linked_list* merge(linked_list* l1, linked_list* l2) {
+    linked_list* merged = new linked_list();
+    node* c1 = l1->head;
+    node* c2 = l2->head;
+    while (c1 != nullptr && c2 != nullptr) {
+        if (c1->get_data() < c2->get_data()) {
+            merged->push_back(c1->get_data());
+            c1 = c1->get_next_node();
+        } else {
+            merged->push_back(c2->get_data());
+            c2 = c2->get_next_node();
+        }
+    }
+    while (c1 != nullptr) {
+        merged->push_back(c1->get_data());
+        c1 = c1->get_next_node();
+    }
+    while (c2 != nullptr) {
+        merged->push_back(c2->get_data());
+        c2 = c2->get_next_node();
+    }
+    return merged;
+}
+
 int main() {
-    linked_list l = linked_list(5);
-    l.push_back(6);
-    l.push_front(7);
-    l.deleteByValue(7);
-    l.display();
-    l.reverse_list();
-    l.display();
+    linked_list l1 = linked_list(5);
+    linked_list l2 = linked_list(1);
+    l1.push_back(6);
+    l2.push_back(2);
+    l2.push_back(3);
+    l1.display();
+    l2.display();
+    linked_list* l3 = merge(&l1, &l2);
+    l3->display();
     return 0;
 }
